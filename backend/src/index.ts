16eb27defe9144457a2b272bae490111e79d8222
jitenderswami <jitenderswami68017@gmail.com>
2024-12-27 21:34:33 +0530

    cross origin base urls

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://status-page-application-fe.vercel.app",
+      "http://localhost:5173",
+    ],
+  })
+);
 app.use(express.json());
 
 // Routes
--- frontend/src/configs/AppConfigs.ts
+++ frontend/src/configs/AppConfigs.ts
@@ -1,27 +1,27 @@
 type AppConfig = {
-    apiBaseUrl: string;
-}
+  apiBaseUrl: string;
+};
 
 const productionConfig: AppConfig = {
-    apiBaseUrl: "https://api.statussphere.com"
-}
+  apiBaseUrl: "https://statuspageapplication.onrender.com",
+};
 
 const stagingConfig: AppConfig = {
-    apiBaseUrl: "http://localhost:3000"
-}
+  apiBaseUrl: "http://localhost:3000",
+};
 
 function getConfig(): AppConfig {
-    // @ts-ignore
-    const environment = import.meta.env.VITE_ENV || "test";
+  // @ts-ignore
+  const environment = import.meta.env.VITE_ENV || "test";
 
-    switch (environment) {
-        case "production":
-            return productionConfig;
-        case "test":
-            return stagingConfig;
-        default:
-            return stagingConfig;
-    }
+  switch (environment) {
+    case "production":
+      return productionConfig;
+    case "test":
+      return stagingConfig;
+    default:
+      return stagingConfig;
+  }
 }
 
 export const appConfig: AppConfig = getConfig();
